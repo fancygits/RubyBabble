fa903346bfb36dc9aa35590506fbcd69744f4ac0
James Luke Johnson <jjohn144@my.westga.edu>
2019-01-08 19:08:01 -0600

    Continued work on tile_bag

--- tile_bag.rb
+++ tile_bag.rb
@@ -1,40 +1,54 @@
 class TileBag
 	
-	tiles = []
-	
-	def initialize
-		9.times { tiles.push(:A) }
-	end
-	
-	# Tile Values
-	values = {
-		A: 1,
-		B: 2,
-		C: 3,
-		D: 2,
-		E: 1,
-		F: 4,
-		G: 2,
-		H: 4,
-		I: 1,
-		J: 8,
-		K: 5,
-		L: 1,
-		M: 3,
-		N: 1,
-		O: 1,
-		P: 3,
-		Q: 10,
-		R: 1,
-		S: 1,
-		T: 1,
-		U: 1,
-		V: 4,
-		W: 4,
-		X: 8,
-		Y: 4,
-		Z: 1
-	}
+	attr_reader :tiles
+  
+  def initialize
+    @tiles = []
+    fill_bag
+  end
+  
+  def fill_bag
+    tiles.push(:K, :J, :X, :Q, :Z)
+    2.times { tiles.push(:B, :C, :M, :P, :F, :H, :V, :W, :Y) }
+    3.times { tiles.push(:G) }
+    4.times { tiles.push(:L, :S, :U, :D) }
+    6.times { tiles.push(:N, :R, :T) }
+    8.times { tiles.push(:O) }
+    9.times { tiles.push(:A, :I) }
+    12.times { tiles.push(:E) }
+  end
+  
+  def get_value letter
+    # Tile Values
+    values = {
+      A: 1,
+      B: 2,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 1
+    }
+    return values[letter]
+  end
 	
 end
-
