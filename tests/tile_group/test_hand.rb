a7e886e822031d9db6403a35ec8f09bae533cb1c
James Luke Johnson <jjohn144@my.westga.edu>
2019-01-15 21:36:51 -0600

    Created and tested the TileGroup#hand method using TDD.

---
+++ tests/tile_group/test_hand.rb
@@ -0,0 +1,51 @@
+require 'minitest/autorun'
+require_relative '../../tile_group.rb'
+
+# This class tests the hand method of TileGroup.
+# Author:: James Luke Johnson (mailto:jjohn144@my.westga.edu)
+# Version:: 2019.1.15
+class TestHand < MiniTest::Test
+
+  def setup # :nodoc:
+    @group = TileGroup.new
+  end
+  
+  # Tests that an empty TileGroup will convert to an empty string
+  def test_convert_empty_group_to_string
+    assert_equal '', @group.hand
+  end
+  
+  # Tests that a TileGroup with one tile will convert to a string
+  def test_convert_single_tile_group_to_string
+    @group.append(:A)
+    assert_equal 'A', @group.hand
+  end
+  
+  # Tests that a TileGroup with many tiles will convert to a string
+  def test_convert_multi_tile_group_to_string
+    @group.append(:V)
+    @group.append(:I)
+    @group.append(:C)
+    @group.append(:T)
+    @group.append(:O)
+    @group.append(:R)
+    @group.append(:Y)
+    assert_equal 'VICTORY', @group.hand
+  end
+  
+  # Tests that a TileGroup with duplicate tiles will convert to a string
+  def test_convert_multi_tile_group_with_duplicates_to_string
+    @group.append(:M)
+    @group.append(:I)
+    @group.append(:S)
+    @group.append(:S)
+    @group.append(:I)
+    @group.append(:S)
+    @group.append(:S)
+    @group.append(:I)
+    @group.append(:P)
+    @group.append(:P)
+    @group.append(:I)
+    assert_equal 'MISSISSIPPI', @group.hand
+  end
+end
--- tile_group.rb
+++ tile_group.rb
@@ -1,23 +1,37 @@
+# This class represents a group of _Scrabble_ tiles drawn from a TileBag.
+# Author:: James Luke Johnson (mailto:jjohn144@my.westga.edu)
+# Version:: 2019.1.15
 class TileGroup
   
   attr_reader :tiles
   
-  def initialize
+  # Creates an empty TileGroup
+  def initialize 
     @tiles = []
   end
   
+  # Appends a new *Tile* to the TileGroup
   def append(tile)
     raise "Tile has invalid format." unless is_tile_valid?(tile)
     @tiles.push(tile)
   end
   
+  # Removes a *Tile* from the TileGroup
   def remove(tile)
     raise "Tile not found." if (@tiles.index(tile) == nil)
     @tiles.delete_at(@tiles.index(tile))
   end
   
+  # Returns a string that is the concatenation of all Tiles' string values
+  def hand
+    hand = ''
+    @tiles.each { |tile| hand += tile.to_s }
+    return hand
+  end
+  
   private
   
+  # Checks that a *Tile* is a Symbol and exactly 1 uppercase character.
   def is_tile_valid?(tile)
     tile.is_a?(Symbol) && tile.length == 1 && /[[:upper:]]/.match(tile)
   end
